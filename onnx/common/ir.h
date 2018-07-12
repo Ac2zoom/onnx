41fdd8d3337244fe09ad636c8a410d327ed27749
Akshay Chalana <akshayc@fb.com>
2018-07-12 15:44:55 -0700

    Better constructor?

--- onnx/common/ir.h
+++ onnx/common/ir.h
@@ -789,7 +789,7 @@ struct OpSetID {
     version = proto.version();
   }
 
-  OpSetID();
+  OpSetID() {};
 
   std::string toString() const {
     return "$" + domain + "$" + std::to_string(version);
